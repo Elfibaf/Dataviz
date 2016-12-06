2fb836a779b3cb066cc389d868b42504a185bb21
Elfibaf <fabien.robin@hotmail.fr>
2016-12-06 01:01:19 +0100

    ss

--- map.js
+++ map.js
@@ -404,7 +404,7 @@ $(function () {
                                     column: {
                                         stacking: 'normal'
                                     },
-                                    /*series : {
+                                    series : {
                                         events : {
                                             legendItemClick: function (event) {
                                                 var color = this.color;
@@ -423,7 +423,7 @@ $(function () {
                                                 }
                                             }
                                         }
-                                    }*/
+                                    }
                                 },
                                 series: creerBars(data_bar, event.point)
                             })
@@ -469,7 +469,7 @@ $(function () {
                 stacking: 'normal',
                 
             },
-            /*series: {
+            series: {
                 events : {
                     legendItemClick: function (event) {
                         var color = this.color;
@@ -488,7 +488,7 @@ $(function () {
                         }
                     }
                 }
-            }*/
+            }
             
         },
         series: [{
